0ea9f3ebe6a0820d2427b32979579bd5d84044fb
Chris Myers <c486733@cttlc.com>
2024-01-26 08:33:42 -0500

    Basic arg parser structure

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jixer/bip39
 
 go 1.21.5
+
+require github.com/akamensky/argparse v1.4.0
--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/akamensky/argparse"
+)
+
 func main() {
-	println("Hello, World!")
+	parser := argparse.NewParser("bip39", "Command line utility written in Go for generating and parsing bip-39 seed phrases")
+
+	// Create subparser
+	help := parser.Flag("h", "help", &argparse.Options{Required: false, Help: "Show help information for this command"})
+
+	// Parse input
+	err := parser.Parse(os.Args)
+	if err != nil {
+		// In case of error print error and print usage
+		// This can also be done by passing -h or --help flags
+		fmt.Print(parser.Usage(err))
+		os.Exit(1)
+	}
+
+	if *help {
+		fmt.Print(parser.Usage(nil))
+	}
 }
